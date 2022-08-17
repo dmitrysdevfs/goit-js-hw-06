bf827649b8761c876af9219a0b446cbe8a24808f
Dmitry Salyakin <d.salyakin@gmail.com>
2022-08-17 03:27:51 +0300

    Task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonRef = document.querySelector('.change-color');
+const textLabel = document.querySelector('.color');
+
+buttonRef.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  textLabel.textContent = getRandomHexColor();
+}
+
