19d0aeb5222cc2222d9dcdb31af103911d849563
Dmitry Salyakin <d.salyakin@gmail.com>
2022-08-17 03:01:39 +0300

    Task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  const formData = {
+    email: formElements.email.value,
+    password: formElements.password.value,
+  };
+
+  formData.email && formData.password ?
+    (event.currentTarget.reset(), console.log(formData)) :
+    alert('Fill in all fields!');
+}
+
+   // const formData = new FormData(event.currentTarget);
+
+  // formData.forEach((value, name) => {
+  //   console.log('name', name);
+  //   console.log('value', value);
+
+  // });
+
 
